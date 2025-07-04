2f70468c3dd081ffafdaef9ac904cee921409e0f
Fernando Aguilar <146612624+faguilar02@users.noreply.github.com>
2025-07-04 00:53:13 -0500

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,2 +1,11 @@
 #!/bin/bash
+# Verificar si PORT está definido (requerido por Render)
+if [ -z "$PORT" ]; then
+  export PORT=8000  # Valor por defecto para entornos locales
+fi
+
+# Forzar modo CPU para evitar errores de CUDA
+export CUDA_VISIBLE_DEVICES=""
+
+# Iniciar la aplicación
 uvicorn app.main:app --host 0.0.0.0 --port $PORT
